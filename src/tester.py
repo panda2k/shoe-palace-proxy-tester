447890fa3e465a709ce6227e15fa1f3afb4ea9af
panda2k <thelowkeypanda@gmail.com>
2020-08-17 10:11:06 -0700

    add upgraded error handling and logging

--- src/tester.py
+++ src/tester.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from selenium.common.exceptions import NoSuchElementException, WebDriverException, UnexpectedAlertPresentException
 from selenium.webdriver.firefox.options import Options
 import pandas as pd
 from datetime import datetime
@@ -40,6 +40,12 @@ def load_proxies():
     with open(PROXY_LIST, 'r') as f:
         return [line.rstrip('\n') for line in f]
 
+def log_to_file(log_file_path, message):
+    with open(log_file_path, 'a') as f:
+        f.write(message + '\n')
+    
+    return
+
 def test_proxies(output_path, thread_name):
     print(f'Thread {str(thread_name)} started')
     driver = get_driver()
@@ -68,22 +74,24 @@ def test_proxies(output_path, thread_name):
                     test_data['result'].append('Dead')
                 else:
                     test_data['result'].append('Unknown')
+                    driver.save_screenshot(f'{proxy.split(":")[1]}.png') # optional debug
+                    log_to_file(os.path.join(os.getcwd(), f'logs\\{proxy.split(":")[1]}.txt'), str(e)) # optional debut
             else:
-                time.sleep(0.1)
-                if '501 Backend Timeout' in driver.page_source:
-                    test_data['result'].append('501')
-                else:
-                    try:
-                        driver.find_element_by_id('cf-wrapper')
-                        test_data['result'].append('Bad')
-                    except NoSuchElementException:
-                        test_data['result'].append('Good')
-                        #driver.save_screenshot(f'{proxy.split(":")[1]}.png') # optional debug
+                try:
+                    if '501 Backend Timeout' in driver.page_source:
+                        test_data['result'].append('501')
+                    else:
+                        try:
+                            driver.find_element_by_id('cf-wrapper')
+                            test_data['result'].append('Bad')
+                        except NoSuchElementException:
+                            test_data['result'].append('Good')
+                except UnexpectedAlertPresentException:
+                    test_data['result'].append('Auth error')
     
             dataframe = pd.DataFrame(test_data)
             dataframe.to_csv(output_path, index = False, mode = 'a', header = False)
             print(f'Thread {str(thread_name)} has finished testing {proxy}. Results of test: {test_data["result"][-1]}. Took {round(time.time() - start_time, 3)} seconds.')
-            time.sleep(5)
 
 def main():
     global proxies
